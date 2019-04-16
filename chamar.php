6038a56834e03b15ddd0b64e5ff02b96e3c8341a
Rafael <rafaworksi@outlook.com>
2019-04-16 19:03:08 -0300

    concluido

--- chamar.php
+++ chamar.php
@@ -4,7 +4,7 @@ include("conexao.php");
 
 //criando a query de consulta ao status em atendimento que é igual a 2
 $teste1= mysqli_query(
-	$conn, "UPDATE atende  SET statusAtende=3 WHERE statusAtende=2 ORDER BY codAtende ASC LIMIT 1" );
+	$conn, "UPDATE atende  SET statusAtende=3 WHERE statusAtende=2 ORDER BY codAtende ASC LIMIT 1" 	);
 
 $teste2= mysqli_query(
 	$conn, "UPDATE atende  SET statusAtende=2 WHERE statusAtende=1  LIMIT 1 ");
