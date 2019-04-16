3f09e789ff7976a5e737e193b2d67549df21f300
Renan D <renan.decarlo@yahoo.com.br>
2019-04-16 14:38:24 -0300

    Remove query não utilizada

--- chamar_modo_atendente.php
+++ chamar_modo_atendente.php
@@ -1,9 +1,6 @@
 <?php 
 	include("conexao.php");  
-
-	$consulta= mysqli_query($conn, "SELECT  statusAtende FROM atende") or die( mysqli_error($cx) );//caso haja um erro na consulta 
-	$aux = mysqli_fetch_assoc($consulta);
-			
+		
 	//criando a query de consulta ao status em atendimento que é igual a 2
 	$teste1= mysqli_query($conn, "UPDATE atende  SET statusAtende=3 WHERE statusAtende=2 ORDER BY codAtende ASC LIMIT 1" );
 	$teste2= mysqli_query($conn, "UPDATE atende  SET statusAtende=2 WHERE statusAtende=1  LIMIT 1 ");
