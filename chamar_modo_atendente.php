7899816fb00bd75bc3dedf752f3ea47562782a6f
Renan D <renan.decarlo@yahoo.com.br>
2019-04-16 14:40:53 -0300

    Conserta o header
    O header deve sempre vir antes de qualquer output

--- chamar_modo_atendente.php
+++ chamar_modo_atendente.php
@@ -7,9 +7,8 @@
 
 
 	if (mysqli_query($conn, $sql)) {
-    	echo "New record created successfully";
     	header("Location: chamaatendimento.php");
-
+		echo "New record created successfully";
 	} else {
 		echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 	}
